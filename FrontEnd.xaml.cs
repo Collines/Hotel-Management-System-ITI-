d970b84378f9a22d68569badeb0fc00c63293070
collines <mohammed.mido9719@gmail.com>
2023-03-30 19:53:21 +0200

    Implemented Login and Reservation tab basic designs, Combobox fill upon picking month showing(29,30,31) days depending on each month

---
+++ FrontEnd.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class FrontEnd : Window
+    {
+        public enum Months:byte
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December,
+        }
+        public FrontEnd()
+        {
+            InitializeComponent();
+            InitializeComboBox();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void InitializeComboBox()
+        {
+            int Days = 0;
+            ComboBoxItem selectedItem = (ComboBoxItem)BDMonthCombo?.SelectedItem;
+            if(Enum.TryParse<Months>(selectedItem?.Content?.ToString(), out Months M))
+            {
+                switch (M)
+                {
+                    case Months.January:
+                        Days = 30;
+                        break;
+                    case Months.February:
+                        Days = 29;
+                        break;
+                    case Months.March:
+                        Days = 31;
+                        break;
+                    case Months.April:
+                        Days = 30;
+                        break;
+                    case Months.May:
+                        Days = 31;
+                        break;
+                    case Months.June:
+                        Days = 30;
+                        break;
+                    case Months.July:
+                        Days = 31;
+                        break;
+                    case Months.August:
+                        Days = 31;
+                        break;
+                    case Months.September:
+                        Days = 30;
+                        break;
+                    case Months.October:
+                        Days = 31;
+                        break;
+                    case Months.November:
+                        Days = 30;
+                        break;
+                    case Months.December:
+                        Days = 31;
+                        break;
+                    default:
+                        Days = 30;
+                        break;
+                }
+
+            }
+            if(Days>0)
+            {
+                BDDayCombo?.Items.Clear();
+                for(int i=1;i<=Days;i++)
+                {
+                    ComboBoxItem item = new();
+                    item.Content = i;
+                    BDDayCombo?.Items.Add(i);
+                }
+            }
+        }
+
+        private void MonthComboChange(object sender, SelectionChangedEventArgs e)
+        {
+            InitializeComboBox();
+        }
+    }
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CloseBtn(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ClearPlaceholder(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox Tbx)
+            {
+                if (Tbx?.Text == "Username")
+                    Tbx.Text = "";
+            }
+            else if (sender is PasswordBox PwdBox)
+                {
+                    if (PwdBox?.Password == "Password")
+                        PwdBox.Password = "";
+                }
+        }
+
+        private void RemovedFocus(object sender, RoutedEventArgs e)
+        {
+            if(sender is TextBox Tbx)
+            {
+                if (Tbx?.Text.Length == 0)
+                    Tbx.Text = "Username";
+            }
+            else if(sender is PasswordBox PwdBox)
+            {
+                if (PwdBox?.Password.Length == 0)
+                    PwdBox.Password = "Password";
+            }
+        }
+    }
+}
