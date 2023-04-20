c5724cf6c2022ae2187f6a8324f2899f3b30d3dc
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-20 02:36:10 +0200

    Updated FrontEnd&RoomService Layout, Added Rooms & Cities SQL Data

---
+++ Cities.sql
@@ -0,0 +1,120 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Collections.Generic;
+
+----------------------------------------------------------
+--Host:                         DESKTOP - VREBPAN\MSSQLSERVER17
+-- Server version:               Microsoft SQL Server 2022 (RTM-GDR) (KB5021522) - 16.0.1050.5
+-- Server OS:                    Windows 10 Pro 10.0 <X64> (Build 22621: )
+--HeidiSQL Version: 12.1.0.6537
+----------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES  */
+;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+;
+/*!40103 SET TIME_ZONE='+00:00' */
+;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+;
+
+--Dumping data for table FRONTEND.Cities: 79 rows
+/*!40000 ALTER TABLE "Cities" DISABLE KEYS */;
+INSERT INTO "Cities" ("Name") VALUES
+    ('Cairo'),
+	('Giza'),
+	('Alexandria'),
+	('Madinat as Sadis min October'),
+	('Shubra al Khaymah'),
+	('Al Mansurah'),
+	('Helwan'),
+	('Al MaHallah al Kubra'),
+	('Port Said'),
+	('Suez'),
+	('Tanta'),
+	('Asyut'),
+	('Al Fayyum'),
+	('Az Zaqaziq'),
+	('Ismailia'),
+	('Aswan'),
+	('Kafr ad Dawwar'),
+	('Damanhur'),
+	('Al Minya'),
+	('Damietta'),
+	('Luxor'),
+	('Qina'),
+	('Suhaj'),
+	('Bani Suwayf'),
+	('Shibin al Kawm'),
+	('Al Arish'),
+	('Al Ghardaqah'),
+	('Banha'),
+	('Kafr ash Shaykh'),
+	('Disuq'),
+	('Bilbays'),
+	('Mallawi'),
+	('Idfu'),
+	('Mit Ghamr'),
+	('Munuf'),
+	('Jirja'),
+	('Akhmim'),
+	('Zifta'),
+	('Samalut'),
+	('Manfalut'),
+	('Bani Mazar'),
+	('Armant'),
+	('Maghaghah'),
+	('Kawm Umbu'),
+	('Bur Fuaad'),
+	('Al QuSiyah'),
+	('Rosetta'),
+	('Isna'),
+	('MatruH'),
+	('Abnub'),
+	('Hihya'),
+	('Samannud'),
+	('Dandarah'),
+	('Al Kharjah'),
+	('Al Balyana'),
+	('Matay'),
+	('Naj Hammadi'),
+	('San al Hajar al Qibliyah'),
+	('Dayr Mawas'),
+	('Ihnasya al Madinah'),
+	('Daraw'),
+	('Abu Qir'),
+	('Faraskur'),
+	('Raas Gharib'),
+	('Al Husayniyah'),
+	('Safaja'),
+	('Qiman al Arus'),
+	('Qaha'),
+	('Al Karnak'),
+	('Hirriyat Raznah'),
+	('Al QuSayr'),
+	('Kafr Shukr'),
+	('Siwah'),
+	('Kafr Sad'),
+	('Sharunah'),
+	('At tur'),
+	('RafaH'),
+	('Ash Shaykh Zuwayd'),
+	('Bir al Abd');
+/*!40000 ALTER TABLE "Cities" ENABLE KEYS */
+;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */
+;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */
+;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */
+;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */
+;
